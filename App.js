1f077511c6cddb15f981f64541dcb8c75e8227a9
Derek Roode <30448625+RoodeAwakening@users.noreply.github.com>
2022-06-30 10:43:46 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,63 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import HomeScreen from "./src/screens/HomeScreen";
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const navigator = createStackNavigator(
-  {
-    Home: HomeScreen,
+import HomeScreen from './src/screens/HomeScreen';
+import NewScreen from './src/screens/NewScreen';
+import { Icon } from 'react-native-elements';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#228CBD'
+        },
+        headerTintColor: '#fff'
+      }}
+      initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen} 
+          options={{
+            headerRight: () => (
+              <Icon
+                name='plus'
+                type='feather'
+                color='#fff'
+                style={styles.headerIcon}
+              />
+            )
+          }}
+        />
+        <Stack.Screen name="New" component={NewScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  {
-    initialRouteName: "Home",
-    defaultNavigationOptions: {
-      title: "App",
-    },
+  headerIcon: {
+    marginRight: 10
   }
-);
+});
 
-export default createAppContainer(navigator);
+
+// can add individual styling to page headers 
+        // options={{
+        //   headerStyle: {
+        //     backgroundColor: '#228CBD'
+        //   },
+        //   headerTintColor: '#fff',
+        //   fontWeight: 'bold',
+        // }}
